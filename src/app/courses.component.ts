48027d144882efb542374db75b1e6670e0ddac83
modkhalid <modkhalid5@gmail.com>
2019-01-15 21:27:36 +0530

    class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,26 +5,11 @@ import { EmailService } from './email.service';
 @Component({
     selector: 'courses',
     template: `
-                <h1>{{ title }}</h1>
-                <ul>
-                    <li *ngFor="let course of courses">
-                        {{course}}
-                    </li>
-                </ul>
-                <img src={{src}}/>
-                <img [src]='src'/>
-                <p [attr.id]='clas'>hi</p>
-                <button class ="btn btn-success btn-lg">hi click me</button>
+                
+                <button class ="btn btn-success btn-lg" [class.active]="isActive" [class.add]="isActive">hi click me</button>
              `
 })
 
 export class CoursesComponent{
-    title="list of courses"
-    // courses=['angular','ionic','react native','javascript']
-    courses;
-    clas
-    src='http://lorempixel.com/400/200';
-    constructor(service:CourseService,l:EmailService){
-        this.courses=service.getCourses()
-    }
+    isActive=true
 }
