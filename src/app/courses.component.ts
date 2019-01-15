2e64a33ebf5f6989c50cab6bea5d9909e5c8bebe
modkhalid <modkhalid5@gmail.com>
2019-01-15 21:55:44 +0530

    event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,11 +5,25 @@ import { EmailService } from './email.service';
 @Component({
     selector: 'courses',
     template: `
+               <div (click)="onClick()">
+                    <button (click)="onAnotherClick($event)">
+                        "hi click me
+                    </button>
+                </div>
                 
-                <button class ="btn btn-success btn-lg" [class.active]="isActive" [class.add]="isActive">hi click me</button>
-             `
+    `
 })
 
 export class CoursesComponent{
-    isActive=true
+    // red='red'
+    // isActive=false
+    counter=0;
+    onClick=()=>{
+        console.log("div was clicked"+this.counter)
+        this.counter++;
+    }
+    onAnotherClick=($event)=>{
+        $event.stopPropagation()
+        console.log("button was clicked")
+    }
 }
