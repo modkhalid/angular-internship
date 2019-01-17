ace44c8b101da14209f7acec3f5dc040e7c23cc1
modkhalid <modkhalid5@gmail.com>
2019-01-17 22:53:52 +0530

    custom directives stage2

--- src/app/input.directive.ts
+++ src/app/input.directive.ts
@@ -1,16 +1,20 @@
-import { Directive,HostListener } from '@angular/core';
+import { Directive,HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appInput]'
 })
 export class InputDirective {
-  @HostListener('focus') onFocus=()=>{
-    console.log("Focus event occer")
+  // @HostListener('focus') onFocus=()=>{
+  //   console.log("Focus event occer")
+  // }
+  constructor(private el:ElementRef) { 
+    
   }
-
   @HostListener('blur') onBlur=()=>{
-    console.log("Blur event occer")
+    // console.log("Blur event occer")
+    let value:string=this.el.nativeElement.value;
+    this.el.nativeElement.value=value.toLowerCase();
   }
-  constructor() { }
+  
 
 }
