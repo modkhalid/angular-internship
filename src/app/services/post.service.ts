9c0bfaae3f4fd753fcd3de2224b6cf61a7bae313
modkhalid <modkhalid5@gmail.com>
2019-01-22 00:24:51 +0530

    reusable of error code making a private method in service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,7 +2,7 @@ import { BadError } from './../common/bad-error';
 import { AppError } from './../common/app-error';
 import { NotFoundError } from './../common/not-found-error';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, ErrorHandler } from '@angular/core';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 // import 'rxjs/add/observable/throw'
@@ -23,35 +23,25 @@ export class PostService {
 
   createPost(post){
     return this.http.post(this.url,post)
-      .pipe(
-        catchError((err:Response)=>{
-          if (err.status==400){
-            return throwError(new BadError(err))
-          }
-          return throwError(new AppError(err))
-        })
-      )
+    .pipe(catchError(this.ErrorHandlerMethod))
   }
 
   updatePost(post){
-    return this.http.patch("abcd"+this.url+"/"+post.id,{isread:true}).pipe(
-      
-    )
+    return this.http.patch(this.url,{isread:true})
   }
 
   deletePost(post){
     return this.http.delete("jhdsfjkdhfjkdshjkf"+"/"+4800)
-      .pipe(
-        catchError( err => {
-          if (err.status == 404) {
-              // return Observable.throw(err)
-              return throwError(new NotFoundError(err))
-
-              
-          } 
-          return throwError(new AppError(err))
-     })
-      )
+      .pipe(catchError(this.ErrorHandlerMethod))
     
+  }
+  private ErrorHandlerMethod(error:Response){
+    if (error.status == 404) {
+      return throwError(new NotFoundError(error))   
+    }
+    if (error.status==400){
+      return throwError(new BadError(error))
+    } 
+    return throwError(new AppError(error))
   }
 }
